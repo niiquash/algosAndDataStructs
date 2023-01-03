42d1293bd669d525d7db746ae843b90fb43bf655
niiquash <qua16006@byui.edu>
2023-01-03 10:11:10 -0700

    iterative fibonacci

---
+++ fibonacci.js
@@ -0,0 +1,14 @@
+// Fibonacci algorithm
+function fibonacci(n) {
+    const fib = [0, 1]
+    for (let i = 2; i < n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2]
+    }
+    return fib
+}
+
+console.log(fibonacci(1))
+console.log(fibonacci(2))
+console.log(fibonacci(5))
+console.log(fibonacci(7))
+console.log(fibonacci(10))
