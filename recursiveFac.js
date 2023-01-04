b2ab68c0f0925b531bce34d263b91eb4c3c0bf9b
niiquash <qua16006@byui.edu>
2023-01-03 19:26:37 -0700

    recursive Factorial

---
+++ recursiveFac.js
@@ -0,0 +1,11 @@
+function recursiveFactorial(n) {
+    if (n < 2) {
+        return 1
+    }
+    return n * recursiveFactorial(n - 1)
+}
+
+console.log(recursiveFactorial(0))
+console.log(recursiveFactorial(4))
+console.log(recursiveFactorial(5))
+console.log(recursiveFactorial(8))
